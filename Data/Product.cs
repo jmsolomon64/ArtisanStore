413b46e7fb0366e9c364b4e4355f869ef84f9b6d
Jacob <jmsolomon64@gmail.com>
2022-04-22 10:43:21 -0400

    entity framework packages downloaded and Data class initially built

---
+++ Data/Product.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArtisanStore.Data
+{
+    public class Product 
+    {
+        [Key]
+        public int Id {get; set;}
+        [Required]
+        [MaxLength(100)]
+        public string Name {get; set;}
+        [Required]
+        public int Upc {get; set;}
+        [ForeignKey("Category")]
+        [Required]
+        public int CategoryId {get; set;}
+    }
+}
