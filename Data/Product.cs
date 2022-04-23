634ca8035fab05bf0eb036174d9f9ede583b9ac0
Jacob <jmsolomon64@gmail.com>
2022-04-23 11:07:37 -0400

    Creation of the Advertisement class

--- Data/Product.cs
+++ Data/Product.cs
@@ -15,6 +15,9 @@ namespace ArtisanStore.Data
         [ForeignKey("Category")]
         [Required]
         public int CategoryId {get; set;}
+        [ForeignKey("User")]
+        [Required]
+        public int UserId {get; set;} //tracks the user that is selling this item
         [Required]
         public int OnHand {get; set;} //tracks amount of product in inventory
     }
