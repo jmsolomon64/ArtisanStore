ac5115ef5b81fdf21f7088148d9b45bb5fe76bb0
Jacob <jmsolomon64@gmail.com>
2022-04-23 11:33:52 -0400

    Oder updates

--- Data/Order.cs
+++ Data/Order.cs
@@ -9,15 +9,17 @@ namespace ArtisanStore.Data
         public int Id {get; set;}
         [Required]
         [MaxLength(100)]
-        public string Name {get; set;}
+        public string Name {get; set;} 
         [Required]
         [ForeignKey("Product")]
-        public int ProductId {get; set;}
+        public int ProductId {get; set;} 
         [Required]
         [ForeignKey("User")]
         public int UserIdBuy {get; set;}
         [Required]
         [ForeignKey("User")] //we'll probably need to ask Marty how to program a difference between these last two foreign keys
         public int UserIdSell {get; set;}
+        [Required]
+        public double TotalAmount {get; set;} //will help management to track performance 
     }
 }
