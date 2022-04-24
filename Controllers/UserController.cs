e6cee0f64ed1a4b061b6a6766ba036302e76f8ac
Jacob Solomon <jmsolomon64@gmail.com>
2022-04-24 19:23:27 -0400

    Fixed the placement of code from product controller into the User controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,6 +12,26 @@ namespace ArtisanStore.Controllers
             _context = db; 
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromForm] UserEdit model)
+        {
+            if (!ModelState.IsValid) //checks to see if model inserted is valid
+            {
+                return BadRequest(ModelState); //will let user know request was invalid
+            }
+
+            _context.Users.Add(new User()
+            { //All properties will be recieved from form expect Joined which pulls the current date time
+                Name = model.Name,
+                Joined = DateTime.Now,
+                IsVendor = model.isVendor,
+                IsAdvertiser = model.IsAdvertiser
+            });
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
         
     }
 }
